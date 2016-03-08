0ea5df926f57b29688f4b760bfa1ef391cab7314
Rares Tanase <r.tanase@rosoftlab.net>
2016-03-08 16:51:31 +0200

    Open app or AppStore to download

--- plugins/cordova-plugin-vanillanav/www/vn.js
+++ plugins/cordova-plugin-vanillanav/www/vn.js
@@ -1,26 +1,24 @@
 module.exports.navigate = function(venueId, referenceId){
-	if(device.platform === 'Android') {
-		var successCallback = function(){
+	// if(device.platform === 'Android') {
+	// 	var successCallback = function(){
 
-		};
-		var errorCallback = successCallback;
-		cordova.exec(successCallback, errorCallback, "VanillaNav", "navigate", [venueId, referenceId]);
-	} else if(device.platform === 'iOS') {
+	// 	};
+	// 	var errorCallback = successCallback;
+	// 	cordova.exec(successCallback, errorCallback, "VanillaNav", "navigate", [venueId, referenceId]);
+	// } else if(device.platform === 'iOS') {
 		var scheme = "vanillanav://navigateTo?venueId=" + venueId + "&referenceId=" + referenceId;
 
 		appAvailability.check(
-    		scheme,       // URI Scheme or Package Name
+    		'vanillanav://',       // URI Scheme or Package Name
     		function() {  // Success callback
     			console.log(scheme + ' is available :)');
-    			//window.location=scheme;
     			window.open(scheme, '_system');
     		},
 		    function() {  // Error callback
 		    	console.log(scheme + ' is not available :(');
-		    	//window.location="https://itunes.apple.com/ro/app/vanillanav/id1007116984?mt=8";
-		    	window.open('https://itunes.apple.com/ro/app/vanillanav/id1007116984?mt=8', '_system');
+                window.open('itms-apps://itunes.apple.com/ro/app/vanillanav/id1007116984?mt=8', '_system');
 		    }
 		);
-	}
+	// }
 
 }
