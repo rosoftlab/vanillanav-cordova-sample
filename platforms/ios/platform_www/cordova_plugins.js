0e41629a4d26ba92e4bd57f8ef601f7362cbc0d6
Rares Tanase <r.tanase@rosoftlab.net>
2016-03-08 16:36:28 +0200

    Functional js code

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-appavailability/www/AppAvailability.js",
+        "id": "cordova-plugin-appavailability.AppAvailability",
+        "pluginId": "cordova-plugin-appavailability",
+        "clobbers": [
+            "appAvailability"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-vanillanav/www/vn.js",
+        "id": "cordova-plugin-vanillanav.device",
+        "pluginId": "cordova-plugin-vanillanav",
+        "clobbers": [
+            "vanillaNav"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
--- plugins/cordova-plugin-vanillanav/www/vn.js
+++ plugins/cordova-plugin-vanillanav/www/vn.js
@@ -1,7 +1,26 @@
 module.exports.navigate = function(venueId, referenceId){
-  var successCallback = function(){
+	if(device.platform === 'Android') {
+		var successCallback = function(){
 
-  };
-  var errorCallback = successCallback;
-  cordova.exec(successCallback, errorCallback, "VanillaNav", "navigate", [venueId, referenceId]);
+		};
+		var errorCallback = successCallback;
+		cordova.exec(successCallback, errorCallback, "VanillaNav", "navigate", [venueId, referenceId]);
+	} else if(device.platform === 'iOS') {
+		var scheme = "vanillanav://navigateTo?venueId=" + venueId + "&referenceId=" + referenceId;
+
+		appAvailability.check(
+    		scheme,       // URI Scheme or Package Name
+    		function() {  // Success callback
+    			console.log(scheme + ' is available :)');
+    			//window.location=scheme;
+    			window.open(scheme, '_system');
+    		},
+		    function() {  // Error callback
+		    	console.log(scheme + ' is not available :(');
+		    	//window.location="https://itunes.apple.com/ro/app/vanillanav/id1007116984?mt=8";
+		    	window.open('https://itunes.apple.com/ro/app/vanillanav/id1007116984?mt=8', '_system');
+		    }
+		);
+	}
+
 }
